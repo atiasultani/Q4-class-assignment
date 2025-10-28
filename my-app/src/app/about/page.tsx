0e697c7c59860dad6dee9a87c898e719cf17317a
atiasultani <160133182+atiasultani@users.noreply.github.com>
2025-10-29 02:40:57 +0500

    ok

---
+++ my-app/src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { topics } from "@/lib/data";
+
+export default function about() {
+  return (
+    <section className="px-12 py-8">
+      <h1 className="text-3xl font-bold mb-6 mt-6 font-mono">Saturday 2:00pm to 5:00pm</h1>
+      <h2 className="text-2xl font-semibold mb-9 mt-8">GIAIC STUDENT</h2>
+
+      <div className="grid sm:grid-cols-2 gap-6">
+                  
+            <h3 className="text-xl font-bold">Female</h3>
+        </div>
+    </section>
+  );}
